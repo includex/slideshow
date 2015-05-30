707d9771a50eabe2ba68e490e8b96cc9f75d2368
Edward <edward.jeong@daumkakao.com>
2015-05-30 13:45:10 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+var slide = require('./routes/slide');
+var multer  = require('multer')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(multer({ dest: './uploads/'}))
+
+app.use('/files', express.static('files'));
+app.use('/public', express.static('public'));
+
+app.use('/', routes);
+app.use('/users', users);
+app.use('/slide', slide);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+
+var server = app.listen(3000, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('ex : %s %s', host, port);
+});
+
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection',function(socket){
+  console.log('Message from client');
+  socket.emit('sign',{msg:'Welcome !'});
+  socket.on('fromclient',function(data){
+    socket.broadcast.emit('toclient',data); // 자신을 제외하고 다른 클라이언트에게 보냄
+    socket.emit('toclient',data); // 해당 클라이언트에게만 보냄. 다른 클라이언트에 보낼려면?
+    console.log('Message from client :'+data.msg);
+  })
+});
---
+++ routes/slide.js
@@ -0,0 +1,84 @@
+var express = require('express');
+var fs = require('fs');
+
+
+var router = express.Router();
+var TOOL_PATH = "/Users/includex/tool";
+var STORAGE_PATH = "/Users/includex/WebstormProjects/slide/files"
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('slide_create', { title: 'Create Slide' });
+});
+
+router.get('/show/:id', function(req, res, next) {
+    var id= req.params.id;
+    var path = STORAGE_PATH + '/' + id;
+    var isExist = fs.existsSync(path);
+    if(isExist === false){
+        res.redirect('/slide/notexist');
+        return;
+    }
+
+    var glob = require("glob")
+    glob(path + '/*.jpg', [], function (er, files) {
+        var arr = [];
+        for(var i = 1; i <= files.length; i++){
+            arr.push(i);
+        }
+        res.render('slide_show_slide', { slideid: id, slides: arr });
+    });
+});
+
+router.get('/notexist', function(req, res, next) {
+    res.render('upload_complete', { title: 'Expres22s' });
+});
+
+router.post('/create', function(req, res, next) {
+    fs.readFile(req.files.file.path, function(error, data){
+        var skey = Date.now() + '_' + Math.round(Math.random()* 1000000000);
+        var path = STORAGE_PATH + '/' + skey;
+        var filepath = path + '/' + ".pdf";
+
+        fs.mkdirSync(path);
+
+        fs.writeFile(filepath, data, function(error){
+            if(error){
+                throw error;
+            }
+
+            var exec = require('child_process').exec;
+            //new run_cmd('java -jar ' + TOOL_PATH + '/pdfbox-app-1.8.9.jar PDFToImage ' + inputpath);
+
+            exec ('java -jar ' + TOOL_PATH + '/pdfbox-app-1.8.9.jar PDFToImage ' + filepath, function(){
+                res.send('' +skey);
+            });
+
+            //pdfToImage(filepath);
+
+
+        });
+    });
+
+
+
+    return false;
+});
+
+function run_cmd(cmd, args) {
+    var exec = require('child_process').exec;
+    exec(cmd);
+    //spawn(cmd, args);
+}
+
+function pdfToImage(inputpath){
+    var exec = require('child_process').exec;
+    //new run_cmd('java -jar ' + TOOL_PATH + '/pdfbox-app-1.8.9.jar PDFToImage ' + inputpath);
+
+    exe ('java -jar ' + TOOL_PATH + '/pdfbox-app-1.8.9.jar PDFToImage ' + inputpath, function(){
+
+    });
+    //new run_cmd('java', ['-jar', TOOL_PATH + '/pdfbox-app-1.8.9.jar', 'PDFToImage', inputpath]);
+}
+
+module.exports = router;
